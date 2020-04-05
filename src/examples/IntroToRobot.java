3ae72c168447ecd5d308d8846afdde2a3d16d743
unknown <nolanbunruh@gmail.com>
2020-04-05 16:43:00 -0700

    added three more squares

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -9,9 +9,27 @@ public class IntroToRobot {
 		r2d2.penDown();
 		r2d2.setPenWidth(20);
 		r2d2.setSpeed(5);
+		r2d2.setRandomPenColor();
 		for (int i = 0; i < 4; i++) {
 			r2d2.move(100);
 			r2d2.turn(90);
+		}
+		r2d2.setRandomPenColor();
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(-100);
+			r2d2.turn(-90);
+		}
+		r2d2.setRandomPenColor();
+		r2d2.turn(-90);
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(100);
+			r2d2.turn(90);
+		}
+		r2d2.turn(-180);
+		r2d2.setRandomPenColor();
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(-100);
+			r2d2.turn(-90);
 		}
 
 	}
